2088e4e0a4b2136fea9fc1f7599d420fa64ba387
AnatolyTS <72595542+AnatolyTS@users.noreply.github.com>
2020-11-03 18:22:20 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int bmi = service.calculate(180, 90);
+        double bmi = service.calculate(180, 90);
         System.out.println(bmi);
         double bmi1 = service.calculate(170, 70);
         System.out.println(bmi1);
-        int bmi2 = service.calculate(200, 50);
+        double bmi2 = service.calculate(200, 50);
         System.out.println(bmi2);
         double bmi3 = service.calculate(156, 80);
         System.out.println(bmi3);
