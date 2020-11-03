1f3b15e51ee8eb6235579f1fc71f66b3d45e813d
AnatolyTS <72595542+AnatolyTS@users.noreply.github.com>
2020-11-03 18:19:54 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ public class Main {
         BmiService service = new BmiService();
         int bmi = service.calculate(180, 90);
         System.out.println(bmi);
-        int bmi1 = service.calculate(170, 70);
+        double bmi1 = service.calculate(170, 70);
         System.out.println(bmi1);
         int bmi2 = service.calculate(200, 50);
         System.out.println(bmi2);
-        int bmi3 = service.calculate(156, 80);
+        double bmi3 = service.calculate(156, 80);
         System.out.println(bmi3);
     }
 }
